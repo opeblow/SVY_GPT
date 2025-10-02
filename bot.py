23c74774ff65bde09bd06e5e0ea9b1d262a399b7
opeblow <opeblow2021@gmail.com>
2025-10-02 19:48:15 +0100

    BOTS CODE

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ async def error_handler(update,context):
 
 # Main Function
 def main():
-    app=Application.bui().token(TELEGRAM_TOKEN).build()
+    app=Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND,handle_message))
     app.add_error_handler(error_handler)
 
